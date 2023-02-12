f5844bb11dddce570654c9eff9ae0f8b97484a8d
loppillo <franpobletron@gmail.com>
2023-02-12 16:36:15 -0300

    borrar

--- cafe/src/main/java/com/example/demo/model/dao/IHorarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.entity.Horarios;
-
-public interface IHorarioDao extends CrudRepository<Horarios, Long> {
-
-}
--- cafe/src/main/java/com/example/demo/model/dao/ITragosDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.entity.Tragos;
-
-public interface ITragosDao extends CrudRepository<Tragos , Long>{
-
-}
