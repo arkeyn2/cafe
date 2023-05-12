97037feee72898a6f7dec027e01691d9b2db9d89
arkeyn2 <arkeyn@outlook.cl>
2023-05-11 22:23:42 -0400

    sql

--- cafe/src/main/resources/import.sql
+++ cafe/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO rol (rol_nombre) VALUES ('ROLE_ADMIN'), ('ROLE_CHICA'), ('ROLE_MESERO'), ('ROLE_BAILARINA');
+INSERT INTO rol (rol_nombre) VALUES ('ROLE_ADMIN'), ('ROLE_CHICA'), ('ROLE_MESERO'), ('ROLE_MESERO');
 
 INSERT INTO public.usuario(apellido, email, nombre, nombre_usuario, password, rut) VALUES ('alexis', 'a@a.a','alexis' ,'alexis' , '$2a$10$g1NiG2AiQpTxg/a8HkvB.ObqopcO1QCk76jywtelrv17BMpulR4F2', '123456');
 	
@@ -23,3 +23,6 @@ INSERT INTO public.horario(	id, estado, horario_entrada, horario_salida) VALUES 
 
 INSERT INTO public.control(	id, fecha, hora_entrada, hora_salida, total_horaextra, horarios_id, usuario_id)	VALUES (1, '2016-02-12','20:00', '01:00', '3', 1, 1);
 
+INSERT INTO public.servicios(id, detalle_servicio, estado_servicio, iva, metodo_pago, nombre_servicio, pieza, tipo_servicio, valor)	VALUES (1,'perro', 'activo', 12356, 'efe', 'asd', 's', 'sad', 23654);
+
+INSERT INTO public.acciones(id, adelanto, caja, comisiones, dia_activo, estado, estado_de_pago, fecha_dia, pendiente, tipo_accion, total_ganado, servicio_id, tragos_id, usuario_id) VALUES (1, 1234, 1235, 4556, '2023-05-11', 'a', 'p', '2023-05-11', 's', 's', 12345, 1, 1, 1);
