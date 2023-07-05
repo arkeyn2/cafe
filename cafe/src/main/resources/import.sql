2e32644318d28905b4bd8bd623c8cbebff9c5177
arkeyn-git <arkeyn@outlook.cl>
2023-07-05 13:53:17 -0400

    perfiles

--- cafe/src/main/resources/import.sql
+++ cafe/src/main/resources/import.sql
@@ -1,10 +1,10 @@
-INSERT INTO rol (rol_nombre) VALUES ('ROLE_ADMIN'), ('ROLE_CHICA'), ('ROLE_MESERO'), ('ROLE_MESERO');
+INSERT INTO rol (rol_nombre) VALUES ('ROLE_ADMIN'), ('ROLE_CHICA'), ('ROLE_MESERO'), ('ROLE_BAILARIONA');
 
 INSERT INTO public.usuario(apellido, email,estado ,foto, nombre, nombre_usuario, password, rut) VALUES ('alexis', 'a@a.a','inactivo','5b1ab969-8c7d-4a0b-a26b-4ffd24227183_2023-04-01-19_21_55.8080100.jpg','alexis' ,'alexis' , '$2a$10$g1NiG2AiQpTxg/a8HkvB.ObqopcO1QCk76jywtelrv17BMpulR4F2', '123456');
 	
-INSERT INTO public.usuario(apellido, email,estado ,foto, nombre, nombre_usuario, password, rut) VALUES ('secretaria', 's@s.s','inactivo','869182d4-a988-426a-9269-38994c869d96_sona_lingerie_by_lifeisafiction_dfcy0nv-fullview.jpg', 'secretaria', 'secretaria', '$2a$10$uNq0xm359UvjKgS4L0Ad5umETvBYtCNFysqokAkisxOCUGC7XsDri', '1234569');
+INSERT INTO public.usuario(apellido, email,estado ,foto, nombre, nombre_usuario, password, rut) VALUES ('mesero', 's@s.s','inactivo','869182d4-a988-426a-9269-38994c869d96_sona_lingerie_by_lifeisafiction_dfcy0nv-fullview.jpg', 'mesero', 'mesero', '$2a$10$1LBaC082dl.Y.WTZpyRoNesxNi/uxtqRXq.1NufvNHyAO2sNnCYJy', '1234569');
 	
-INSERT INTO public.usuario(apellido, email,estado ,foto, nombre, nombre_usuario, password, rut) VALUES ('trabajador', 'u@u.u','inactivo','a21315e8-39e2-43f7-b7ef-71d3d95487cd_280443466_606102284214511_4304388199877668259_n(1).jpg', 'trabajador', 'trabajador','$2a$10$GrxgW1ydycwaoNGdMWrykulnU8w90ay2oS/p4G60mkMx3hbJRlNfC', '98765432');
+INSERT INTO public.usuario(apellido, email,estado ,foto, nombre, nombre_usuario, password, rut) VALUES ('chica', 'u@u.u','inactivo','a21315e8-39e2-43f7-b7ef-71d3d95487cd_280443466_606102284214511_4304388199877668259_n(1).jpg', 'chica', 'chica','$2a$10$yAO7lcrRfVhpPvdvGucVQONwlewtyA.tvQ1zAXPTVjZj.hJ.UMpbm', '98765432');
 
 
 INSERT INTO public.usuario_rol(	usuario_id, rol_id)	VALUES (1, 1);
